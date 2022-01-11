bc419f85959a165d09a0bfc8d1f06000880adb42
Bruno Luiz Silva <contact@brunoluiz.net>
2022-01-11 23:08:33 +0000

    feat: add button for convert

--- src/js/content.js
+++ src/js/content.js
@@ -1,35 +1,56 @@
 const addClickHandlersAllElements = () => {
   document.querySelectorAll("img").forEach((img) => {
     if (!img.src.includes(".gif")) return;
-    img.addEventListener("click", () => start(img));
+    const gif = img;
+    const { width, height, style, src } = gif;
+
+    const wrapper = document.createElement("div");
+    wrapper.style = style;
+    wrapper.style.position = "relative";
+    wrapper.appendChild(gif.cloneNode());
+
+    const convert = document.createElement("div");
+    convert.style.fontFamily = "sans-serif";
+    convert.style.letterSpacing = -0.25;
+    convert.style.position = "absolute";
+    convert.style.bottom = "0";
+    convert.style.left = "0";
+    convert.style.padding = `10px 18px`;
+    convert.style.background = "rgba(255,255,255,.8)";
+    convert.style.borderTopRightRadius = "5px";
+    convert.style.fontWeight = 700;
+    convert.style.cursor = "pointer";
+    convert.style.color = "#000";
+    convert.textContent = "Convert to video";
+    convert.onclick = (e) => {
+      convert.remove();
+      start(wrapper, { width, height, style, src });
+      return false;
+    };
+
+    wrapper.appendChild(convert);
+    gif.parentNode.replaceChild(wrapper, gif);
   });
 };
 
-const start = async (obj) => {
-  const gif = obj;
-  const { width, height, style } = gif;
+const start = async (container, { width, height, style, src }) => {
+  const spinner = document.createElement("img");
+  spinner.src = chrome.runtime.getURL("src/img/loading.gif");
+  spinner.style.background = "transparent";
+  spinner.style.height = "22px";
 
-  const wrapper = document.createElement("div");
-  wrapper.style = style;
-  wrapper.style.position = "relative";
-  wrapper.style.height = `${height}px`;
-  wrapper.style.width = `${width}px`;
-
-  const loading = document.createElement("img");
-  loading.src = chrome.runtime.getURL("src/img/loading.gif");
+  const loading = document.createElement("div");
   loading.style.position = "absolute";
   loading.style.bottom = "0";
   loading.style.left = "0";
-  loading.style.padding = `10px`;
-  loading.style.height = "32px";
+  loading.style.padding = `10px 12px`;
   loading.style.background = "rgba(255,255,255,.8)";
   loading.style.borderTopRightRadius = "5px";
+  loading.appendChild(spinner);
 
-  wrapper.appendChild(gif.cloneNode());
-  wrapper.appendChild(loading);
-  gif.parentNode.replaceChild(wrapper, gif);
+  container.appendChild(loading);
 
-  chrome.extension.sendMessage({ src: gif.src }, async function ({ blobText }) {
+  chrome.extension.sendMessage({ src }, async function ({ blobText }) {
     const blob = await (await fetch(blobText)).blob();
     const url = URL.createObjectURL(blob);
 
@@ -45,7 +66,7 @@ const start = async (obj) => {
     source.type = "video/mp4";
 
     video.appendChild(source);
-    wrapper.parentNode.replaceChild(video, wrapper);
+    container.parentNode.replaceChild(video, container);
   });
 };
 
