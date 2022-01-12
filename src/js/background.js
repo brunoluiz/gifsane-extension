e96194ecd184fd44344a03e8045e69611a5531e7
Bruno Luiz Silva <contact@brunoluiz.net>
2022-01-12 00:05:00 +0000

    feat: add request middleware to allow blobs to be injected on a page

--- src/js/background.js
+++ src/js/background.js
@@ -1,3 +1,6 @@
+const isCSPHeader = (headerName) =>
+  headerName === "CONTENT-SECURITY-POLICY" || headerName === "X-WEBKIT-CSP";
+
 const createFileName = (ext) =>
   `${Math.floor(Math.random() * Math.pow(2, 32) + 1)}.${ext}`;
 let ffmpeg = undefined;
@@ -58,3 +61,33 @@ chrome.extension.onMessage.addListener(function (
   process();
   return true;
 });
+
+// Listens on new request
+chrome.webRequest.onHeadersReceived.addListener(
+  (details) => {
+    for (let i = 0; i < details.responseHeaders.length; i += 1) {
+      if (!isCSPHeader(details.responseHeaders[i].name.toUpperCase())) {
+        continue;
+      }
+
+      const values = details.responseHeaders[i].value.split(";");
+      details.responseHeaders[i].value = values
+        .map((v) => {
+          if (!v.includes("media-src")) return v;
+
+          // required to allow script to inject converted video
+          return `${v} blob:`;
+        })
+        .join(";");
+    }
+
+    return {
+      responseHeaders: details.responseHeaders,
+    };
+  },
+  {
+    urls: ["*://*/*"],
+    types: ["main_frame"],
+  },
+  ["blocking", "responseHeaders"]
+);
